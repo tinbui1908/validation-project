bd2216d9955bd789385096d8044edd3df19013d8
Thanh Duy Nguyễn <18120171@student.hcmus.edu.vn>
2021-12-28 19:33:20 +0700

    add class Validation and ConstraintViolation

---
+++ ValidationFramework/MyFramework/ConstraintViolation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFramework
+{
+    class ConstraintViolation
+    {
+        private string _property;
+
+        private object _value;
+
+        private bool _status;
+
+        private string _message;
+
+        public string Property
+        {
+            get => _property;
+
+            set => _property = value;
+        }
+
+        public object Value
+        {
+            get => _value;
+
+            set => _value = value;
+        }
+
+        public bool Status
+        {
+            get => _status;
+
+            set => _status = value;
+        }
+
+        public string Message
+        {
+            get => _message;
+
+            set => _message = value;
+        }
+    }
+}
---
+++ ValidationFramework/MyFramework/Validation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFramework
+{
+    internal class Validation
+    {
+        private Validation(){}
+    }
+}
