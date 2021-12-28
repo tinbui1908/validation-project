ed5b4a128f3dd317acb5a73384a86ccdb696d01e
Hoàng Khang <nhk25022016@gmail.com>
2021-12-28 16:35:26 +0700

    ValidatorType.cs: add class ValidatorTypeMapping

--- ValidationFramework/MyFramework/ValidatorType.cs
+++ ValidationFramework/MyFramework/ValidatorType.cs
@@ -1,7 +1,10 @@
-﻿namespace MyFramework
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyFramework
 {
     /// <summary>
-    /// Define validator types
+    /// Định nghĩa các kiểu Validator
     /// </summary>
     public enum ValidatorType
     {
@@ -23,5 +26,79 @@
         BLANK,
         // Is a number type
         IS_NUMBER
+    }
+
+    /// <summary>
+    /// Lớp xử lí mapping giữa Validatory type và Annotation type
+    /// </summary>
+    public class ValidatorTypeMapping
+    {
+        /// <summary>
+        /// Dictionary dùng để mapping
+        /// </summary>
+        private static Dictionary<string, ValidatorType> types = new Dictionary<string, ValidatorType>();
+
+        /// <summary>
+        /// Hàm tạo static
+        /// </summary>
+        static ValidatorTypeMapping()
+        {
+            // Nếu dictonary trống, tiến hành khởi tạo các mapping
+            if (types.Count == 0)
+            {
+                InitDictionary();
+            }
+            else { /* Do nothing */ };
+        }
+
+        /// <summary>
+        /// Hàm khởi tạo dictionary
+        /// </summary>
+        private static void InitDictionary()
+        {
+            types.Add("MyAnnotation",
+                        ValidatorType.NOT_NULL);
+            types.Add("MyAnnotation",
+                      ValidatorType.REGEX);
+            types.Add("MyAnnotation",
+                      ValidatorType.PHONE_NUMBER);
+            types.Add("MyAnnotation",
+                      ValidatorType.LENGTH);
+            types.Add("MyAnnotation",
+                      ValidatorType.RANGE);
+            types.Add("MyAnnotation",
+                      ValidatorType.DATE_OF_BIRTH);
+            types.Add("MyAnnotation",
+                      ValidatorType.NO_BLANK);
+            types.Add("MyAnnotation",
+                      ValidatorType.BLANK);
+            types.Add("MyAnnotation",
+                      ValidatorType.IS_NUMBER);
+
+        }
+
+        /// <summary>
+        /// Hàm tìm Validator type tương ứng với Annotation name
+        /// </summary>
+        /// <param name="_annotationName">Tên của annotation</param>
+        /// <returns>Validator type hoặc null</returns>
+        static ValidatorType? GetType(string _annotationName)
+        {
+            if (CheckInclude(_annotationName))
+            {
+                return types[_annotationName];
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Hàm kiểm tra Annotation name đã được mapping chưa (có hỗ trợ không)
+        /// </summary>
+        /// <param name="_annotationName">Tên của annotation</param>
+        /// <returns>Giá trị true/false tương ứng kết quả kiểm tra</returns>
+        private static bool CheckInclude(string _annotationName)
+        {
+            return types.ContainsKey(_annotationName);
+        }
     }
 }
