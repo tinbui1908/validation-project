4909bc0dd2708a7ed76e812aa1d53f828f9d81ac
Hoàng Khang <nhk25022016@gmail.com>
2021-12-28 11:34:54 +0700

    init file ValidatorType.cs

---
+++ ValidationFramework/MyFramework/ValidatorType.cs
@@ -0,0 +1,14 @@
+﻿namespace MyFramework
+{
+    public enum ValidatorType
+    {
+        REGEX,
+        NOT_NULL,
+        PHONE_NUMBER,
+        MAX,
+        MIN,
+        DATE_OF_BIRTH,
+        NOT_BLANK,
+        BLANK
+    }
+}
