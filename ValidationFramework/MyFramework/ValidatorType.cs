1538710578dac4f487126796a0b0690c93b6fade
Hoàng Khang <nhk25022016@gmail.com>
2021-12-28 12:00:45 +0700

    ValidatorType.cs: modify types and add note

--- ValidationFramework/MyFramework/ValidatorType.cs
+++ ValidationFramework/MyFramework/ValidatorType.cs
@@ -1,14 +1,31 @@
 ﻿namespace MyFramework
 {
+    /// <summary>
+    /// Define validator types
+    /// </summary>
     public enum ValidatorType
     {
-        REGEX,
+        // Not null type
         NOT_NULL,
+        // Regular expression type
+        REGEX,
+        // Phone number type
         PHONE_NUMBER,
-        MAX,
-        MIN,
+        // Max length of string type
+        MAX_LENGTH,
+        // Min length of string type
+        MIN_LENGTH,
+        // Scope of string length (from .. to) type
+        LENGTH,
+        // Scope of number (from .. to) type
+        RANGE,
+        // Date (day, month and year) type
         DATE_OF_BIRTH,
-        NOT_BLANK,
-        BLANK
+        // Have no blank ('_') type
+        NO_BLANK,
+        // Have blank(s) ('_') type
+        BLANK,
+        // Is a number type
+        IS_NUMBER
     }
 }
