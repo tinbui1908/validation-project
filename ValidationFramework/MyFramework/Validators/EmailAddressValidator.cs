5ed582b399c88fe7999ce5a78347ff8a676c1173
Hoàng Khang <nhk25022016@gmail.com>
2021-12-28 22:10:04 +0700

    init EmailAddressValidator.cs

---
+++ ValidationFramework/MyFramework/Validators/EmailAddressValidator.cs
@@ -0,0 +1,22 @@
+﻿using MyFramework.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFramework
+{
+    public class EmailAddressValidator : Validator
+    {
+        public override bool CheckInvalid(Attribute attribute, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetMessage(Attribute attribute)
+        {
+            EmailAddressAttribute newAttribute = attribute as EmailAddressAttribute;
+
+            return newAttribute.Message;
+        }
+    }
+}
