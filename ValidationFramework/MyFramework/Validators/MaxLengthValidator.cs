d94a980dec08a9c911378f02791d13bc953d7a7e
tinbui1908 <bhtt190800@gmail.com>
2021-12-29 10:04:14 +0700

    add MinLengthValidator and MaxLengthValidator

---
+++ ValidationFramework/MyFramework/Validators/MaxLengthValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using MyFramework.DataAnnotations;
+using System.Collections.Generic;
+
+namespace MyFramework.Validators
+{
+    public class MaxLengthValidator : Validator
+    {
+        public override Boolean CheckInvalid(Attribute attribute, object value)
+        {
+            MaxLengthAttribute maxLength = attribute as MaxLengthAttribute;
+
+            return maxLength.MaxLength > ((String)value).Length;
+        }
+
+        public override string GetMessage(Attribute attribute)
+        {
+            MaxLengthAttribute maxLength = attribute as MaxLengthAttribute;
+            return maxLength.Message;
+        }
+    }
+}
+
---
+++ ValidationFramework/MyFramework/Validators/MinLengthValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using MyFramework.DataAnnotations;
+using System.Collections.Generic;
+
+namespace MyFramework.Validators
+{
+	public class MinLengthValidator: Validator
+	{
+        public override Boolean CheckInvalid(Attribute attribute, object value)
+        {
+            MinLengthAttribute minLength = attribute as MinLengthAttribute;
+
+            return minLength.MinLength < ((String)value).Length;
+        }
+
+        public override string GetMessage(Attribute attribute)
+        {
+            MinLengthAttribute minLength = attribute as MinLengthAttribute;
+            return minLength.Message;
+        }
+    }
+}
