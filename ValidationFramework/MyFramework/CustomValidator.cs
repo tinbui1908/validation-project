2b85d63ff8bf88fef44594f0799ed52d400be942
Hoàng Khang <nhk25022016@gmail.com>
2022-01-01 16:00:51 +0700

    inti file CustomValidator.cs

---
+++ ValidationFramework/MyFramework/CustomValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyFramework.ValidationClasses;
+
+namespace MyFramework.Validators
+{
+    public class CustomValidator : Validator
+    {
+        public override bool CheckInvalid(Attribute attribute, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetMessage(Attribute attribute)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
